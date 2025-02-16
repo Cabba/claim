18ad692eda4a639a2dd24ec912030a790199373b
Federico Cabassi <federico.cabassi@dilica.it>
2025-02-16 22:49:53 +0100

    added doc and changed api

--- claim.go
+++ claim.go
@@ -1,14 +1,34 @@
+// claim is a simple package to implement defensive programming and handle unrecoverable errors.
 package claim
 
-// Specify a precondition
-func Pre(c bool, msg string) {
+// Specify a precondition.
+// If the condition fails the application will panic.
+// Use preconditions to express conditions that must hold true,
+// prefer returning errors for recoverable errors.
+//
+// Example:
+//
+//	func divide(a, b int) {
+//		claim.Pre(b != 0)
+//	}
+func Pre(c bool) {
 	if !c {
-		panic(msg)
+		panic("precondition not satisfied")
 	}
 }
 
-func Post(c bool, msg string) {
+// Specify a postcondition.
+// If the condition fails the application will panic.
+// Use postconditions to express conditions that must hold true,
+// prefer returning errors for recoverable errors.
+//
+// Example:
+//
+//	func (d *Door) Open() {
+//		defer claim.Post(d.open)
+//	}
+func Post(c bool) {
 	if !c {
-		panic(msg)
+		panic("postcondition not satisfied")
 	}
 }
